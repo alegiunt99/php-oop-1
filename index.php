669233887c523322b506127f5ac3f937824d29de
Alessandro Giunta <alegiunt99@gmail.com>
2022-05-13 16:52:04 +0200

    Creo la construct con la durata dei film

--- index.php
+++ index.php
@@ -4,19 +4,38 @@
         
         public $name;
         public $genre;
+        public $year;
+
+        
         public $durartion;
         	
-        function __construct() {
+        function __construct($_duration) {
+
+            return $this.$durartion = $_duration;
 
         }
 
     }
 
     // Creo 2 film
-    $hangover = new Movie();
+    $theHangover = new Movie(99);
 
-    $theNotebook = new Movie();
+    $theNotebook = new Movie(123);
 
-    
+    // elementi dei film
+
+    // The Hangover 
+    $theHangover->name = 'The Hangover';
+
+    $theHangover->genre = 'Comedy';
+
+    $theHangover->year = 2009;
+
+    // The Notebook
+    $theNotebook->name = 'The Notebook'
+
+    $theNotebook->genre = 'Romantic'
+
+    $theNotebook->year = 2004;
 
 ?>
